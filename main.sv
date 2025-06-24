380882352eed2cee326cd247a7e9c8a6eaf38143
DanielTirado <danieltirado088@gmail.com>
2025-06-24 16:26:33 -0500

    Check: COLISIONES, PUNTAJES, MOVILIDAD, DISPLAYS. Missing: AMBOS TECLADOS, DIBUJAR PERSONAJES ROM

--- main.sv
+++ main.sv
@@ -2,30 +2,27 @@ module main #(FPGAFREQ = 50_000_000)(
 	input logic CLK,
 	input logic nRST,
 	input logic nPBTON,
+	input logic [9:0] sw,
+	input  logic [3:0]  COLUMNAS,
+   output logic [3:0]  FILAS,
+   output logic [3:0]  TECLA,
+   output logic        FLAG,
+   output logic [7:0]  DISP,
 	output logic hsync,
 	output logic vsync,
-	output logic [11:0] rgb_out);
+	output logic [11:0] color,
+   output logic [11:0] rgb_out);
 	
+	 
 	logic RST;
 	logic PBTON;
 	logic clkdiv0;
 	logic [9:0] counter;
 	logic [3:0] units, tens, hundreds;
-	
-	logic PAINTU;
-	logic PAINTD;
-	logic PAINTC;
-	logic PAINT34SEG;
-	logic PAINT_1;
-	logic PAINT_2;
-	logic PAINT_3;
-	logic PAINT_4;
-	logic PAINT_5;
-	
-	//Señales de driver VGA
-	logic [10:0] sig_pixel_x, sig_pixel_y; // 11 bits for pixel counters
-	logic [11:0] color;
-	logic black;
+	logic PAINTU;logic PAINTD;logic PAINTC;
+	logic PAINTU2;logic PAINTD2;logic PAINTC2;
+	logic PAINT34SEG;logic PAINT_1;logic PAINT_2;logic PAINT_3;logic PAINT_4;logic PAINT_5;
+	logic PAINT_6;logic PAINT_7;logic PAINT_8;logic PAINT_9;logic PAINT_10;logic PAINT_11;
 	
 	assign RST = ~nRST;
 	assign PBTON = ~nPBTON;
@@ -33,51 +30,69 @@ module main #(FPGAFREQ = 50_000_000)(
 	///////////Bloque N1 ///////////////////////
 	cntdiv_n #(FPGAFREQ) cntDiv0(CLK,RST,clkdiv0);
    // Instancia driver VGA
-   vga_ctrl_640x480_60Hz vga_ctrl_inst (RST, CLK,color,hsync, vsync, sig_pixel_x,sig_pixel_y,rgb_out,black);
+   vga_ctrl_640x480_60Hz vga_ctrl_inst (RST, CLK,color,hsync, vsync, sig_pixel_x,sig_pixel_y,rgb_out,blanck);
 	// Display 7 segmentos
-	display #(5,20,30,60,10) displayU(sig_pixel_x,sig_pixel_y,units,PAINTU);  //unidades 
-	display #(5,20,30,35,10) displayD(sig_pixel_x,sig_pixel_y,tens,PAINTD);  // decenas
-	display #(5,20,30,10,10) displayC(sig_pixel_x,sig_pixel_y,hundreds,PAINTC);  //centenas
-	// Display 34 segmentos                 34'b111111_111111111111_11111111_11111111
-	display34segm #(5,32) display34a(100,10,34'b111100_111100001111_00000000_00000000,sig_pixel_x,sig_pixel_y,PAINT34SEG);
-	display34segm #(5,32) display34b(125,10,34'b100010_111100000110_01000000_00000001, sig_pixel_x, sig_pixel_y, PAINT_1);   // H
-	display34segm #(5,32) display34c(150,10,34'b111100_111100001100_00001001_00000000, sig_pixel_x, sig_pixel_y, PAINT_2);  // O
-	display34segm #(5,32) display34d(175,10,34'b110011_000011110000_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_3);  // L
-	display34segm #(5,32) display34e(200,10,34'b111100_111100001111_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_4);  // A
-	display34segm #(5,32) display34f(225,10,34'b000000_111101101111_10000001_00000000, sig_pixel_x, sig_pixel_y, PAINT_5);  // A
-	//Pintando los displays de un color  
+	display #(5,20,30,35,10) displayU(sig_pixel_x,sig_pixel_y,units,PAINTU);  //unidades 
+	display #(5,20,30,10,10) displayD(sig_pixel_x,sig_pixel_y,tens,PAINTD);  // decenas
+	
+	// Display 34 segmentos                 34'b111111_111111111111_11111111_11111111  //horizontal_Vertical_diag/_ Diag2
+	display34segm #(3,32) display34g(60,10,34'b111100_111100001111_00000000_00000000,sig_pixel_x,sig_pixel_y,PAINT34SEG);//A
+	display34segm #(3,32) display34h(85,10,34'b100010_111100000110_01000000_00000001, sig_pixel_x, sig_pixel_y, PAINT_7);//D 
+	display34segm #(3,32) display34i(110,10,34'b111100_111100001100_00001001_00000000, sig_pixel_x, sig_pixel_y, PAINT_8);//R
+	display34segm #(3,32) display34j(135,10,34'b110011_000011110000_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_9);//I
+	display34segm #(3,32) display34k(160,10,34'b111100_111100001111_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_10);//A
+	display34segm #(3,32) display34l(185,10,34'b000000_111100000111_01000000_10000000, sig_pixel_x, sig_pixel_y, PAINT_11);//n
+	
+	display #(5,20,30,460,10) displayU2(sig_pixel_x,sig_pixel_y,units,PAINTU2);  //unidades 
+	display #(5,20,30,435,10) displayD2(sig_pixel_x,sig_pixel_y,tens,PAINTD2);  // decenas
+	
+	display34segm #(3,32) display34a(490,10,34'b100010_111100000110_01000000_00000001, sig_pixel_x, sig_pixel_y, PAINT_1);//D
+	display34segm #(3,32) display34b(515,10,34'b111100_111100001111_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_2);//A
+	display34segm #(3,32) display34c(540,10,34'b000000_111100000111_01000000_10000000, sig_pixel_x, sig_pixel_y, PAINT_3);//n
+	display34segm #(3,32) display34d(565,10,34'b110011_000011110000_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_4);//I
+	display34segm #(3,32) display34e(590,10,34'b111011_111100000000_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_5);//E
+	display34segm #(3,32) display34f(615,10,34'b000011_111100000000_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_6);//L
+	//Pintando los displays de un color 
+	
 	always_comb begin
 		color = 12'h000;
-		if (PAINTC) begin
-		  color = 12'h00F;
-		end else if (PAINTD) begin
-		  color = 12'h00F; //azul
-		end else if (PAINTU) begin
-		  color = 12'h00F;
-		end 
-		if (PAINT34SEG) begin
-			color = 12'h8F0;
-		end else if (PAINT_1) begin
-			color = 12'h8F0;
-		end else if (PAINT_2) begin
-			color = 12'h8F0;
-		end else if (PAINT_3) begin
-			color = 12'h8F0;
-		end else if (PAINT_4) begin
-			color = 12'h8F0;
-		end else if (PAINT_5) begin
-			color = 12'h8F0;
-		end 
+		if (PAINTD) color = 12'h00F;
+		else if (PAINTU)color = 12'h00F; //azul
+		
+		if (PAINT_1)color = 12'h8F0;
+		else if (PAINT_2)color = 12'h8F0;
+		else if (PAINT_3)color = 12'h8F0;
+		else if (PAINT_4)color = 12'h8F0;
+		else if (PAINT_5)color = 12'h8F0;
+		else if (PAINT_6)color = 12'h8F0;
+		
+		if (PAINTD2)color = 12'h00F;
+		else if (PAINTU2)color = 12'h00F; //azul
+		
+		if (PAINT34SEG)color = 12'h8F0;
+		else if (PAINT_7)color = 12'h8F0;
+		else if (PAINT_8)color = 12'h8F0;
+		else if (PAINT_9)color = 12'h8F0;
+		else if (PAINT_10)color = 12'h8F0;
+		else if (PAINT_11)color = 12'h8F0; 
+		
 	end
 
 	
 	/////////// Bloque N2////////////////////
+	
+	
+	///PRUEBA DE PUNTUACION JUGADOR1
+	//if(posxbala1<=320 && (posxbala2==(posxbala1+anchoBala1)) && (posyBala1+==posyBala2)) counterJ1+1;//agregar los altos de las balas
+	//else if(posxbala1==(625-(anchoBala1))counterJ1+2; //golpa en el muro
+	
+	
 	always_ff @(posedge clkdiv0, posedge RST) begin
 		if(RST)begin
 			counter <= 10'b00_0000_0000;
 		end else begin
 			if(PBTON)begin
-				if(counter == 999)
+				if(counter == 99)
 					counter <= 10'b00_0000_0000;
 				else
 					counter <= counter + 1'b1;
@@ -90,7 +105,7 @@ module main #(FPGAFREQ = 50_000_000)(
 	always_comb begin
 		units = 4'(counter % 10'd10);
 		tens = 4'((counter / 10'd10)%10'd10);
-		hundreds = 4'(counter / 10'd100);
+		//hundreds = 4'(counter / 10'd100);
 	end
 		
 endmodule
--- topTestVGA.sv
+++ topTestVGA.sv
@@ -1,7 +1,7 @@
 //========================
 // MÓDULO TOP - CON 2 JUGADORES
 //========================
-module topTestVGA (
+module topTestVGA #(FPGAFREQ = 50_000_000) (
     input  logic clk,
     input  logic nreset_tv,
     input  logic [9:0] sw,
@@ -18,57 +18,103 @@ module topTestVGA (
     output logic [7:0] DISP
 );
 
-    // VGA y parámetros de pantalla
-    logic [10:0] sig_pixel_x, sig_pixel_y;
-    logic [10:0] x_max = 11'd640, y_max = 11'd480;
-    logic [11:0] rgb_color;
-    logic        blank;
-    logic        reset_tv;
-    logic        rst_key;
-    logic        clkout;
+   // VGA y parámetros de pantalla
+   logic [10:0] sig_pixel_x, sig_pixel_y;
+   logic [10:0] x_max = 11'd640, y_max = 11'd480, margin_top = 11'd60;
+   logic [11:0] rgb_color;
+   logic        blank;
+   logic        reset_tv;
+   logic        rst_key;
+   logic        clkout;
 
-    // JUGADOR 1 (original - teclas 5, 0, A)
-    logic [10:0] player1_x = 11'd25, player1_y = 11'd80;
-    logic [10:0] player1_w = 11'd50, player1_h = 11'd100;
-    logic        paint_player1;
+   // JUGADOR 1 (original - teclas 5, 0, A)
+   logic [10:0] player1_x = 11'd25, player1_y = 11'd80;
+   logic [10:0] player1_w = 11'd50, player1_h = 11'd50;
+   logic        paint_player1;
+	 
+	// MURO JUGADOR 1
+	logic [10:0] wall1_x = 11'd5;
+   logic [10:0] wall_w = 11'd10, wall_h = 11'd500, wall_y = 11'd60;
+   logic paint_wall1;
+	 
+	// MURO JUGADOR 2
+	logic [10:0] wall2_x = 11'd625;
+   logic paint_wall2;
+	 
+	// JUGADOR 2 (nuevo - teclas 3, 9, B)
+   logic [10:0] player2_x = 11'd565, player2_y = 11'd80;  // Lado derecho
+   logic [10:0] player2_w = 11'd50,  player2_h = 11'd50;
+   logic        paint_player2;
 
-    // JUGADOR 2 (nuevo - teclas 3, 9, B)
-    logic [10:0] player2_x = 11'd565, player2_y = 11'd80;  // Lado derecho
-    logic [10:0] player2_w = 11'd50,  player2_h = 11'd100;
-    logic        paint_player2;
+   // BALA JUGADOR 1
+   logic [10:0] bullet1_x;
+   logic [10:0] bullet1_y;
+   logic        bullet1_active;
+   logic        paint_bullet1;
+   logic [10:0] bullet_w = 11'd5;
+   logic [10:0] bullet_h = 11'd5;
 
-    // BALA JUGADOR 1
-    logic [10:0] bullet1_x;
-    logic [10:0] bullet1_y;
-    logic        bullet1_active;
-    logic        paint_bullet1;
-    logic [10:0] bullet_w = 11'd10;
-    logic [10:0] bullet_h = 11'd30;
+   // BALA JUGADOR 2
+   logic [10:0] bullet2_x;
+   logic [10:0] bullet2_y;
+   logic        bullet2_active;
+   logic        paint_bullet2;
+	logic 		  CHOQUE_B1 = 1'd0;
+	logic 		  CHOQUE_B2 = 1'd0;
 
-    // BALA JUGADOR 2
-    logic [10:0] bullet2_x;
-    logic [10:0] bullet2_y;
-    logic        bullet2_active;
-    logic        paint_bullet2;
-	 logic 		  CHOQUE = 1'd0;
+   // Velocidades
+   logic [10:0] vel_p = 11'd10;  // Velocidad jugadores
+   logic [10:0] vel_b = 11'd1;   // Velocidad balas
+	
+	// Displays
+	logic clkdiv0;
+	logic [7:0] score1 = 8'd0;
+	logic [7:0] score2 = 8'd0;	 
+	logic [3:0] units_1, units_2, tens_1, tens_2;
+	logic PAINTU;logic PAINTD;logic PAINTC;
+	logic PAINTU2;logic PAINTD2;logic PAINTC2;
+	logic PAINT34SEG;logic PAINT_1;logic PAINT_2;logic PAINT_3;logic PAINT_4;logic PAINT_5;
+	logic PAINT_6;logic PAINT_7;logic PAINT_8;logic PAINT_9;logic PAINT_10;logic PAINT_11;
+	
+	
+   assign reset_tv = ~nreset_tv;
+   assign rst_key = ~nreset_key;
 
-    // Velocidades
-    logic [10:0] vel_p = 11'd10;  // Velocidad jugadores
-    logic [10:0] vel_b = 11'd1;   // Velocidad balas
-
-    assign reset_tv = ~nreset_tv;
-    assign rst_key = ~nreset_key;
-
-    // VGA Driver
-    vga_ctrl_640x480_60Hz vga_ctrl_inst (
-        .rst(reset_tv), .clk(clk), .rgb_in(rgb_color),
-        .HS(hsync), .VS(vsync),
-        .hcount(sig_pixel_x), .vcount(sig_pixel_y),
-        .rgb_out(rgb_out), .blank(blank)
-    );
-
+   // VGA Driver
+   vga_ctrl_640x480_60Hz vga_ctrl_inst (
+       .rst(reset_tv), .clk(clk), .rgb_in(rgb_color),
+       .HS(hsync), .VS(vsync),
+       .hcount(sig_pixel_x), .vcount(sig_pixel_y),
+       .rgb_out(rgb_out), .blank(blank)
+   );
+	 
+	cntdiv_n #(FPGAFREQ) cntDiv0(CLK,RST,clkdiv0);
+	 
+	// Display 7 segmentos
+	display #(5,20,30,35,10) displayU(sig_pixel_x,sig_pixel_y,units_1,PAINTU);  //unidades 
+	display #(5,20,30,10,10) displayD(sig_pixel_x,sig_pixel_y,tens_1,PAINTD);  // decenas
+	
+	// Display 34 segmentos                 34'b111111_111111111111_11111111_11111111  //horizontal_Vertical_diag/_ Diag2
+	display34segm #(3,32) display34g(60,10,34'b111100_111100001111_00000000_00000000,sig_pixel_x,sig_pixel_y,PAINT34SEG);//A
+	display34segm #(3,32) display34h(85,10,34'b100010_111100000110_01000000_00000001, sig_pixel_x, sig_pixel_y, PAINT_7);//D 
+	display34segm #(3,32) display34i(110,10,34'b111100_111100001100_00001001_00000000, sig_pixel_x, sig_pixel_y, PAINT_8);//R
+	display34segm #(3,32) display34j(135,10,34'b110011_000011110000_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_9);//I
+	display34segm #(3,32) display34k(160,10,34'b111100_111100001111_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_10);//A
+	display34segm #(3,32) display34l(185,10,34'b000000_111100000111_01000000_10000000, sig_pixel_x, sig_pixel_y, PAINT_11);//n
+	
+	display #(5,20,30,460,10) displayU2(sig_pixel_x,sig_pixel_y,units_2,PAINTU2);  //unidades 
+	display #(5,20,30,435,10) displayD2(sig_pixel_x,sig_pixel_y,tens_2,PAINTD2);  // decenas
+	
+	display34segm #(3,32) display34a(490,10,34'b100010_111100000110_01000000_00000001, sig_pixel_x, sig_pixel_y, PAINT_1);//D
+	display34segm #(3,32) display34b(515,10,34'b111100_111100001111_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_2);//A
+	display34segm #(3,32) display34c(540,10,34'b000000_111100000111_01000000_10000000, sig_pixel_x, sig_pixel_y, PAINT_3);//n
+	display34segm #(3,32) display34d(565,10,34'b110011_000011110000_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_4);//I
+	display34segm #(3,32) display34e(590,10,34'b111011_111100000000_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_5);//E
+	display34segm #(3,32) display34f(615,10,34'b000011_111100000000_00000000_00000000, sig_pixel_x, sig_pixel_y, PAINT_6);//L
+	//Pintando los displays de un color 
+	 
     // Clock lento para teclado
-    cntdiv_n clk_divisor(clk, rst_key, clkout);
+    cntdiv_n #(100_000) clk_divisor(clk, rst_key, clkout);
 
     // Driver teclado
     Driver_Teclado teclado1 (clk, nreset_key, COLUMNAS, FILAS, TECLA, FLAG, DISP);
@@ -81,12 +127,12 @@ module topTestVGA (
         if (FLAG && ~FLAG_d) begin
             case (TECLA)
                 // JUGADOR 1 - Controles originales
-                4'd5: if (player1_y > 11'd10)                player1_y <= player1_y - vel_p;  // Subir
-                4'd0: if (player1_y + player1_h < y_max)     player1_y <= player1_y + vel_p;  // Bajar
+                4'd5: if (player1_y > margin_top)				player1_y <= player1_y - vel_p;  // Subir
+                4'd0: if (player1_y + player1_h < y_max)		player1_y <= player1_y + vel_p;  // Bajar
                 
                 // JUGADOR 2 - Nuevos controles
-                4'd3: if (player2_y > 11'd10)                player2_y <= player2_y - vel_p;  // Subir (tecla 3)
-                4'd9: if (player2_y + player2_h < y_max)     player2_y <= player2_y + vel_p;  // Bajar (tecla 9)
+                4'd3: if (player2_y > margin_top)				player2_y <= player2_y - vel_p;  // Subir (tecla 3)
+                4'd9: if (player2_y + player2_h < y_max)		player2_y <= player2_y + vel_p;  // Bajar (tecla 9)
             endcase
         end
     end
@@ -95,7 +141,7 @@ module topTestVGA (
     always_ff @(posedge clkout) begin
         // BALA JUGADOR 1 (se mueve hacia la derecha)
         if (bullet1_active) begin
-            if (bullet1_x + bullet_w >= x_max || CHOQUE) begin
+            if (bullet1_x + bullet_w >= x_max || CHOQUE_B1) begin
                 bullet1_active <= 0;
             end else begin
                 bullet1_x <= bullet1_x + vel_b;
@@ -104,7 +150,7 @@ module topTestVGA (
 
         // BALA JUGADOR 2 (se mueve hacia la izquierda)
         if (bullet2_active) begin
-            if (bullet2_x <= 11'd0 || CHOQUE) begin
+            if (bullet2_x <= 11'd0 || CHOQUE_B2) begin
                 bullet2_active <= 0;
             end else begin
                 bullet2_x <= bullet2_x - vel_b;
@@ -129,28 +175,94 @@ module topTestVGA (
                 end
             endcase
         end
+		  
+		  if (CHOQUE_B2) begin
+				if (bullet2_x <= wall1_x+wall_w) begin
+					score2 <= score2 + 2;
+				end else if ((bullet2_x < player2_x) && (bullet2_x > x_max/2) ) score2 <= score2+1;
+			end
+			
+		  if (CHOQUE_B1) begin
+				if (bullet1_x+bullet_w >= wall2_x) begin
+					score1 <= score1 + 2;
+				end else if ((bullet1_x > player1_x) && (bullet1_x < x_max/2) ) score1 <= score1+1;  
+		  end
 	end
 
 	
 	// DETECCIÓN DE COLISIONES - LÓGICA COMBINACIONAL
     always_comb begin
-        CHOQUE = 0;  // Por defecto no hay choque
-        
+        CHOQUE_B1 = 0;  // Por defecto no hay choque
+        CHOQUE_B2 = 0;  // Por defecto no hay choque        
+		  
         if (bullet1_active && bullet2_active) begin
-            // Detección de colisión rectangular (AABB - Axis Aligned Bounding Box)
+				// Detección de colisión rectangular
             if ((bullet1_x < bullet2_x + bullet_w) &&           // Bala1 izquierda < Bala2 derecha
                 (bullet1_x + bullet_w > bullet2_x) &&           // Bala1 derecha > Bala2 izquierda
                 (bullet1_y < bullet2_y + bullet_h) &&           // Bala1 arriba < Bala2 abajo
                 (bullet1_y + bullet_h > bullet2_y)) begin       // Bala1 abajo > Bala2 arriba
-                CHOQUE = 1;
+                CHOQUE_B1 = 1;
+					 CHOQUE_B2 = 1;
+//				if (bullet2_x > y_max/2) begin 
+//					score2 <= score2+1;
+//				end else if(bullet2_x < y_max/2) score1 <= score1+1;
             end
-        end
+		  end
+			
+			if (bullet1_active) begin
+				// Detección de colisión bala 1 con jugador 2
+				if ((bullet1_x+bullet_w == player2_x) && 
+						 (bullet1_y >= player2_y)    && 
+						 (bullet1_y <= player2_y+player2_h)) begin
+						 CHOQUE_B1 = 1;
+				// Detección de colisión bala 1 con muro 2
+				end else if (bullet1_x+bullet_w == wall2_x) CHOQUE_B1 = 1;
+			end
+			
+			if (bullet2_active) begin
+				// Detección de colisión bala 2 con jugador 1
+				if ((bullet2_x == player1_x+player1_w) && 
+					 (bullet2_y >= player1_y)    				&& 
+					 (bullet2_y <= player1_y+player1_h)) begin
+					 CHOQUE_B2 = 1;
+				// Detección de colisión bala 2 con muro 1
+				end else if (bullet2_x == wall1_x+wall_w) CHOQUE_B2 = 1;
+			end
     end
 	 
 	 
     // Salida de color
     always_comb begin
         rgb_color = 12'h000;  // Fondo negro
+        
+		if (PAINTD) rgb_color = 12'h00F;
+		else if (PAINTU)rgb_color = 12'h00F; //azul
+		
+		if (PAINT_1)rgb_color = 12'h8F0;
+		else if (PAINT_2)rgb_color = 12'h8F0;
+		else if (PAINT_3)rgb_color = 12'h8F0;
+		else if (PAINT_4)rgb_color = 12'h8F0;
+		else if (PAINT_5)rgb_color = 12'h8F0;
+		else if (PAINT_6)rgb_color = 12'h8F0;
+		
+		if (PAINTD2)rgb_color = 12'h00F;
+		else if (PAINTU2)rgb_color = 12'h00F; //azul
+		
+		if (PAINT34SEG)rgb_color = 12'h8F0;
+		else if (PAINT_7)rgb_color = 12'h8F0;
+		else if (PAINT_8)rgb_color = 12'h8F0;
+		else if (PAINT_9)rgb_color = 12'h8F0;
+		else if (PAINT_10)rgb_color = 12'h8F0;
+		else if (PAINT_11)rgb_color = 12'h8F0; 
+		
+		  
+		  // Muro 1 - Color azul
+        if (paint_wall1) 
+            rgb_color = 12'h00F;
+        
+		  // Muro 2 - Color verde
+        if (paint_wall2) 
+            rgb_color = 12'h0F0;
         
         // Jugador 1 - Color azul
         if (paint_player1) 
@@ -168,7 +280,18 @@ module topTestVGA (
         if (paint_bullet2 && bullet2_active) 
             rgb_color = 12'hFF0;
     end
+	
+	
+	always_comb begin
+		
+		units_1 = 4'(score1 % 10'd10);
+		tens_1  = 4'((score1 / 10'd10)%10'd10);
+		
+		units_2 = 4'(score2 % 10'd10);
+		tens_2  = 4'((score2 / 10'd10)%10'd10);
 
+	end
+		
     // Dibujar jugador 1
     draw_square player1_draw (
         .pix_x(sig_pixel_x), .pix_y(sig_pixel_y),
@@ -199,6 +322,24 @@ module topTestVGA (
         .x1_limit(bullet2_x), .x2_limit(bullet2_x + bullet_w),
         .y1_limit(bullet2_y), .y2_limit(bullet2_y + bullet_h),
         .PAINT(paint_bullet2)
+    );
+	 
+	 
+    // Dibujar muro jugador 1
+    draw_square wall1_draw (
+        .pix_x(sig_pixel_x), .pix_y(sig_pixel_y),
+        .x1_limit(wall1_x), .x2_limit(wall1_x + wall_w),
+        .y1_limit(wall_y), .y2_limit(wall_y + wall_h),
+        .PAINT(paint_wall1)
+    );
+	 
+	 
+    // Dibujar muro jugador 2
+    draw_square wall2_draw (
+        .pix_x(sig_pixel_x), .pix_y(sig_pixel_y),
+        .x1_limit(wall2_x), .x2_limit(wall2_x + wall_w),
+        .y1_limit(wall_y), .y2_limit(wall_y + wall_h),
+        .PAINT(paint_wall2)
     );
 
 endmodule
